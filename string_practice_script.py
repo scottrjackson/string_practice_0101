c8ef09a111cc781a5cd9e8f2f9179de2d5703840
Scott Russell Jackson <scottrj@umd.edu>
2024-04-23 14:29:42 -0400

    initial commit, started with some review of how quotes work in strings.

---
+++ string_practice_script.py
@@ -0,0 +1,19 @@
+# practice with string and string methods
+
+"This is a string"
+
+'This is also a string'
+
+"This isn't a bad idea."
+
+# 'This is a bad idea, isn't it?'
+
+'She told me, "go there"'
+# "She told me, "go there"" # this is bad
+
+report = "She told me, \"don't go there\"."
+print(report)
+
+print("Here's\na string\nwith some\nline returns\n\n")
+print("\u2660")
+
