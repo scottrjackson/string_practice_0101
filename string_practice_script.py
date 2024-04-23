3ae1cb8fc841c18e17ef5843c67ae58c392bcfa1
Scott Jackson <scottrj@umd.edu>
2024-04-23 14:43:05 -0400

    added some stuff on find and index

--- string_practice_script.py
+++ string_practice_script.py
@@ -1,5 +1,7 @@
 # practice with string and string methods
 
+# Quotes make strings
+# Use escape (backslash) to make a quote "plain text"
 "This is a string"
 
 'This is also a string'
@@ -16,4 +18,18 @@ print(report)
 
 print("Here's\na string\nwith some\nline returns\n\n")
 print("\u2660")
+
+######################
+# find and index
+
+"Here's a short sentence.".count("s")
+example_sentence = "Here's a short sentence."
+example_sentence.count("s")
+example_sentence.find("s")
+print(example_sentence[5])
+example_sentence.find("s", 6) # looking for the next s
+
+example_sentence.find("z")
+example_sentence.index("s", 6) # works the same when it's there
+example_sentence.index("z")
 
