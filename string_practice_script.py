7cb4a71eaed179f52a6a57800aab52f34a053582
Scott Jackson <scottrj@umd.edu>
2024-04-25 14:46:26 -0400

    added examples for startswith, endswith, and join

--- string_practice_script.py
+++ string_practice_script.py
@@ -63,3 +63,39 @@ spam_song.count("spam")
 ###############################
 # starting 4/25 lecture with endswith and startswith
 
+import os
+files_here = os.listdir()
+print(files_here)
+type(files_here)
+type(files_here[0])
+
+"file.txt".endswith(".txt")
+"README.md".endswith(".txt")
+
+for file in files_here:
+    print(f"let's check to see if {file} is a TXT file")
+    if file.endswith(".txt"):
+        print(f"Here's the first 100 bytes of the file {file}")
+        with open(file) as file_connection:
+            print(file_connection.read(100))
+
+waitress_lines = []
+with open("spam_song.txt", "r") as file_connection:
+    for line in file_connection:
+        if line.startswith("Waitress"):
+            waitress_lines.append(line)
+
+print(waitress_lines)
+
+print("\n".join(waitress_lines))
+
+example_csv_line = ["name", "email", "gpa", "year"]
+print(example_csv_line[0] + "," +
+      example_csv_line[1] + "," +
+      example_csv_line[2] + "," +
+      example_csv_line[3])
+
+print(",".join(example_csv_line))
+# print(",".join([1, 2, 3, 4])) # this fails because .join() only joins strings
+print(",".join([str(1), str(2), str(3), str(4)]))
+
